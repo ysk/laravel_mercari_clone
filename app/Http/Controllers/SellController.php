6bd59aef6067c531205311fefeeaae3130ea533c
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-08-05 23:53:56 +0900

    N+1問題の解消

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -8,10 +8,17 @@ use App\Models\ItemCondition;
 
 class SellController extends Controller
 {
-    public function showSellForm(){
-        $categories = PrimaryCategory::orderBy('sort_no')->get();
-
-
+    public function showSellForm()
+    {
+        $categories = PrimaryCategory::query()
+        ->with([
+            'secondaryCategories' => function($query)
+            {
+                $query->orderBy('sort_no');
+            }
+        ])
+        ->orderBy('sort_no')
+        ->get();
 
         $conditions = ItemCondition::orderBy('sort_no')->get();
         return view('sell')
